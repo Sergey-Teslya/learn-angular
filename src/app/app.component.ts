3f0719c4820a356270f2f81f4e7a44224bc49163
Sergey Teslya <Teslya147@gmail.com>
2020-12-05 16:28:09 +0200

    Implement input, outpun, data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn-angular';
+  name: string = 'Test name';
+  age: number = 20;
+  click:number = 0;
+
+  setName(event: any): void {
+    this.name = event.target.value;
+  }
+
+  updateClick(increased:boolean){
+    increased == true ? this.click++ : this.click > 0 ? this.click-- : this.click;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { InputChildComponent } from './input-child/input-child.component';
+import { OutputChildComponent } from './output-child/output-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputChildComponent,
+    OutputChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
